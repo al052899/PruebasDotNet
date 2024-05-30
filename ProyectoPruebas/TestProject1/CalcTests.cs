e208dcf9e5ebc570e7b1c452bbefa5416ee14461
RASEC Gonzalez <al052899@uacam.mx>
2024-05-30 13:38:25 -0600

    create

---
+++ ProyectoPruebas/TestProject1/CalcTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void Sum_ShouldCalcAndReturnSameValue()
+        {
+            //Arrange
+            int a = 4;
+            int b= 4;
+            int expected = 8;
+
+            //Act
+            int actual = Calcs.Sum(a, b);
+
+            //Assert
+            Assert.Equals(expected, actual);
+        }
+    }
+}
---
+++ ProyectoPruebas/TestProject1/CollectionSample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Customer
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+public class CollectionSample
+{
+    public static Customer GetCustomer(Customer[] customer, int index)
+    {
+        return customer[index];
+    }
+}
+
